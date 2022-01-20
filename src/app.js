c7dbfd533ff6c16666da4c94ae1bb51361c8b138
PedroParraPerez <pedroparra06091994@gmail.com>
2022-01-20 22:03:30 +0000

    func Random num and symbol generated

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+// Eleccion del numero y del palo
+const Numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const Symbols = ["diamond", "spade", "hearth", "club"];
+
+const Random = arr => {
+  return Math.floor(Math.random() * arr.length);
+};
+console.log(Numbers[Random(Numbers)]);
+console.log(Symbols[Random(Symbols)]);
