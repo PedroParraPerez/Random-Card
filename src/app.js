36c23927464588546b3115c14e94616e16a578e3
PedroParraPerez <pedroparra06091994@gmail.com>
2022-01-21 17:23:40 +0000

    mod function

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import "./style.css";
 window.onload = function() {};
 
 // Eleccion del numero y del palo
-const Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const Symbols = ["diamond", "spade", "hearth", "club"];
+const Numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const Symbols = ["♣", "♠", "♥", "♦"];
 
 const Random = arr => {
   return Math.floor(Math.random() * arr.length);
 };
 console.log(Numbers[Random(Numbers)]);
 console.log(Symbols[Random(Symbols)]);
+
+// ♦ ♥ ♠ ♣
