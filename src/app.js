ef3461f5277e7d9dbbfcd659a46c45867812cc94
PedroParraPerez <pedroparra06091994@gmail.com>
2022-01-21 20:33:39 +0000

    card and dinamismo

--- src/app.js
+++ src/app.js
@@ -2,14 +2,32 @@ import "./style.css";
 
 window.onload = function() {};
 
-// Eleccion del numero y del palo
+// Random num and icon.
 const Numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 const Symbols = ["♣", "♠", "♥", "♦"];
 
 const Random = arr => {
   return Math.floor(Math.random() * arr.length);
 };
-console.log(Numbers[Random(Numbers)]);
-console.log(Symbols[Random(Symbols)]);
 
-// ♦ ♥ ♠ ♣
+const Number_Final = Numbers[Random(Numbers)];
+const Icon_Final = Symbols[Random(Symbols)];
+
+// Icon and Numbers.
+document.querySelector("#Number1").innerHTML = Number_Final;
+document.querySelector("#Number2").innerHTML = Number_Final;
+document.querySelector("#icon").innerHTML = Icon_Final;
+
+const color = v => {
+  if (Icon_Final == "♥" || Icon_Final == "♦") {
+    document.querySelector("#icon").style.color = "red";
+  }
+};
+color();
+// Refresh botton
+
+const button = document.querySelector(".button");
+button.addEventListener("click", e => {
+  e.preventDefault();
+  console.log("enviar");
+});
