92e1af9b3bdee1a1bb5ede4dba2ae20bc07c7cac
PedroParraPerez <pedroparra06091994@gmail.com>
2022-01-22 13:37:27 +0000

    refresh update

--- src/app.js
+++ src/app.js
@@ -14,16 +14,39 @@ const Random = arr => {
 const RandomForRefresh = arr => {
   return Math.floor(Math.random() * arr.length);
 };
-// values for start and refresh, they are diferent
+// values for start, they are diferent
 const Number_Start = Numbers[Random(Numbers)];
 const Icon_Start = Symbols[Random(Symbols)];
-const Number_Refresh = Numbers[RandomForRefresh(Numbers)];
-const Icon_Refresh = Symbols[RandomForRefresh(Symbols)];
 
 // Icon and Numbers querySelector for Start
 document.querySelector("#Number1").innerHTML = Number_Start;
 document.querySelector("#Number2").innerHTML = Number_Start;
 document.querySelector("#icon").innerHTML = Icon_Start;
+
+// Color Start
+let color = v => {
+  if (Icon_Start == "♥" || Icon_Start == "♦") {
+    document.querySelector("#icon").style.color = "red";
+  }
+};
+color();
+
+// Refresh botton
+// PROBLEMA: NO ME CAMBIA EL COLOR DEL ICONO
+
+const buttonRefresh = document.querySelector(".buttonRefresh");
+buttonRefresh.addEventListener("click", e => {
+  e.preventDefault();
+  const RandomForRefresh = arr => {
+    return Math.floor(Math.random() * arr.length);
+  };
+  const RefreshNumber = Numbers[RandomForRefresh(Numbers)];
+  const RefreshSymbol = Symbols[RandomForRefresh(Symbols)];
+
+  document.querySelector("#Number1").innerHTML = RefreshNumber;
+  document.querySelector("#Number2").innerHTML = RefreshNumber;
+  document.querySelector("#icon").innerHTML = RefreshSymbol;
+});
 
 // Select card.
 let Scolor = document.querySelector("#SelectColor");
@@ -34,30 +57,12 @@ buttonselect.addEventListener("click", e => {
   document.querySelector("#Number1").innerHTML = Snumber.value;
   document.querySelector("#Number2").innerHTML = Snumber.value;
   document.querySelector("#icon").innerHTML = Scolor.value;
+
+  //PROBLEMA, LA PRIMERA VEZ QUE ELIJO LA CARTA ME CAMBIA EL COLOR, PERO LUEGP SE MANTIENE ESE COLOR TODO EL RATO
+  const Slcolor = v => {
+    if (Scolor.value == "♥" || Scolor.value == "♦") {
+      document.querySelector("#icon").style.color = "red";
+    }
+  };
+  Slcolor();
 });
-
-// Color (HACER UNA CLASE CON LOS COLORES Y APLICARLE UN CLASSLISTENER)
-let color = v => {
-  if (Icon_Start == "♥" || Icon_Start == "♦") {
-    document.querySelector("#icon").style.color = "red";
-  }
-};
-color();
-
-const Slcolor = v => {
-  if (Scolor.value == "♥" || Scolor.value == "♦") {
-    document.querySelector("#icon").style.color = "red";
-  }
-};
-Slcolor();
-
-// Refresh botton
-
-const buttonRefresh = document.querySelector(".buttonRefresh");
-buttonRefresh.addEventListener("submit", e => {
-  e.preventDefault();
-  document.querySelector("#Number1").innerHTML = Number_Refresh;
-  document.querySelector("#Number2").innerHTML = Number_Refresh;
-  document.querySelector("#icon").innerHTML = Icon_Refresh;
-});
-//   No consigo ejecutar las funciones randoms al presionar el boton pero al pulsar el boton si que reacciona y escribe en la consola enviar
