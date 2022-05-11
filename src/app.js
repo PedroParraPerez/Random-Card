371689e6cda5e2fe012638bc92e986244aa21bb5
PedroParraPerez <pedroparra06091994@gmail.com>
2022-05-11 23:38:17 +0000

    updates

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ window.onload = function() {};
 const Numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 const Symbols = ["♣", "♠", "♥", "♦"];
 
-// values for start
+// values for start....................................................
 
 const Random = arr => {
   return Math.floor(Math.random() * arr.length);
@@ -15,12 +15,12 @@ const Random = arr => {
 const Number_Start = Numbers[Random(Numbers)];
 const Icon_Start = Symbols[Random(Symbols)];
 
-// Icon and Numbers querySelector for Start
+// Icon and Numbers querySelector for Start..........................
 document.querySelector("#Number1").innerHTML = Number_Start;
 document.querySelector("#Number2").innerHTML = Number_Start;
 document.querySelector("#icon").innerHTML = Icon_Start;
 
-// Color Start
+// Color Start.......................................................
 const color = v => {
   if (Icon_Start == "♥" || Icon_Start == "♦") {
     document.querySelector("#icon").style.color = "red";
@@ -28,30 +28,26 @@ const color = v => {
 };
 color();
 
-// Refresh botton
-
+// Refresh botton....................................................
+// target refresh button and card
 const buttonRefresh = document.querySelector(".buttonRefresh");
 const card = document.querySelector(".card");
 
-const REMOVEROTATE = document.querySelector(".REMOVEROTATE");
-REMOVEROTATE.addEventListener("click", e => {
-  const rotate = e => {
-    e.classList.remove("rotate");
-  };
-  rotate(card);
-});
-
+// Events for refresh button
 buttonRefresh.addEventListener("click", e => {
+  // Generate numbers and symbols. And target in html
   const RefreshNumber = Numbers[Random(Numbers)];
   const RefreshSymbol = Symbols[Random(Symbols)];
   document.querySelector("#Number1").innerHTML = RefreshNumber;
   document.querySelector("#Number2").innerHTML = RefreshNumber;
   document.querySelector("#icon").innerHTML = RefreshSymbol;
 
-  const rotate = e => {
-    e.classList.add("rotate");
-  };
-  rotate(card);
+  // remove and add animations
+  card.classList.remove("rotate");
+  window.requestAnimationFrame(function() {
+    card.classList.add("rotate");
+  });
+  // Change color in sumbols
   const color = v => {
     if (RefreshSymbol == "♥" || RefreshSymbol == "♦") {
       document.querySelector("#icon").style.color = "red";
@@ -62,18 +58,10 @@ buttonRefresh.addEventListener("click", e => {
   color();
 });
 
-// Select card.
+// Select card.......................................................
 const Scolor = document.querySelector("#SelectColor");
 const Snumber = document.querySelector("#SelectNumber");
-const buttonselect = document.querySelector("#buttonselect");
-
-const Slcolor = v => {
-  if (Scolor.value == "♥" || Scolor.value == "♦") {
-    document.querySelector("#icon").style.color = "red";
-  } else {
-    document.querySelector("#icon").style.color = "white";
-  }
-};
+const buttonselect = document.querySelector(".buttonselect");
 
 buttonselect.addEventListener("click", e => {
   document.querySelector("#Number1").innerHTML = Snumber.value;
@@ -85,3 +73,11 @@ buttonselect.addEventListener("click", e => {
   });
   Slcolor();
 });
+
+const Slcolor = () => {
+  if (Scolor.value == "♥" || Scolor.value == "♦") {
+    document.querySelector("#icon").style.color = "red";
+  } else {
+    document.querySelector("#icon").style.color = "white";
+  }
+};
